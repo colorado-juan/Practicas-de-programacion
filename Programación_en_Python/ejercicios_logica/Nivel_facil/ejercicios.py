01258ffd75b458dd4b0af54f73e6a8f48f8b9c5c
Juan Colorado <coloradoobando12@gmail.com>
2026-04-30 16:03:23 -0500

    se realizaron los tres primeros ejercicios logicos faciles

---
+++ Programación_en_Python/ejercicios_logica/Nivel_facil/ejercicios.py
@@ -0,0 +1,23 @@
+'''
+ejercicios para la logica de programación en python
+'''
+
+#ejercicios 1
+#Mostrar un mensaje en consola
+print("hellow word \n")
+
+#ejercicio 2
+#Declarar dos variables y sumarlas
+print("sums de dos numeros", end = "\n\n")
+Num1 = 5
+Num2 = 5
+
+print (Num1 + Num2)
+
+#ejercicio 3
+#Convertir grados Celsius a Fahrenheit
+
+print ("Convertifor de grados Celsius a Fahrenheit ", end="\n\n")
+
+Celsius = input("Ingrese los grados Celsius a convertir")
+
